a36e995c08f89257f37db10189a407bf1b1e4748
Payton <payton@laptop.localdomain>
2021-09-20 20:55:50 -0400

    added tests and static files

--- firstsite/polls/models.py
+++ firstsite/polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -6,6 +8,10 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+    
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
